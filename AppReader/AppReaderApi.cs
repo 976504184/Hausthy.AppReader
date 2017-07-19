3b586f4e5ff78f620e08108dbc87d4166c9eef90
lenovo <lenovo@LENOVO-PC>
2017-07-19 21:33:42 +0800

    添加项目文件。

---
+++ AppReader/AppReaderApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppReader
+{
+    public static class Api
+    {
+        public static object GetAppInfo(string appPath)
+        {
+            return null;
+        }
+    }
+}
